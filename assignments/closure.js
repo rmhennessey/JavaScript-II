3c04457a3bbe007e97bb7109efb7b6dabbc84a15
RMH <rory.hennessey@gmail.com>
2019-01-22 18:23:26 -0500

    mvp done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,50 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function holyCaesar(name) {
+  //const name = "Claudius";
+  console.log(`My name is ${name} and I am a fool`);
+  debugger;
+  
+    function revolution() {
+      const rebelYell = "Egads, there is no need to revolt.";
+      console.log(`My fellow Romans, I am ${name}. ${rebelYell}`)
+      debugger;
+      
+      function downWithCaesar() {
+        const emp = "Caesar"
+        console.log(`Down with ${emp}. Down with ${emp}`);
+        debugger;
+      }
+       downWithCaesar();
+   }
+    revolution();
+}
+
+holyCaesar("Claudius");
+
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+
+const counter = () => {
+  
+  let count = 0;
+  function increment(){
+    return count++;
+  }
+  return increment;  
+};
+
+const newCounter = counter();
+
+newCounter();
+
+
+
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
